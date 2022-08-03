088d76c1817369e5b50aec122949edbe86151ead
ItsRealNise <90197402+ItsRealNise@users.noreply.github.com>
2022-08-03 09:18:30 +0700

    Add files via upload

--- rpl-projects/ajaxCariSiswa.php
+++ rpl-projects/ajaxCariSiswa.php
@@ -1,24 +1,28 @@
-<?php
-
-$keyword = $_GET["keyword"];
-
-if($keyword == "irza") {
-	$keyword = "BOOM!!!";
-	echo "<style>
-		* {
-			display: none;
-		}
-
-		body {
-			background-color: black;
-		}
-	</style>";
-} else {
-	$keyword = "Aman...";
-}
-
-?>
-
-<td colspan="5">
-	<h1><?= $keyword; ?></h1>
-</td>
+<html>
+    <head>
+        <meta name="viewport" content="width-scale=device-width, initial-size=1">
+    </head>
+    <body>
+        <?php
+        $data = file_get_contents("http://0.0.0.0:8080/dataSiswaRpl.json");
+        $data = json_decode($data, true);
+        $cek = $_GET["keyword"];
+        $return = [];
+        $i = 0;
+        foreach($data as $index => $siswa) {
+            if(strpos(strtolower($siswa["field3"]), strtolower($cek)) !== false){
+                $return[$i] = $data[$index];
+                $i++;
+            }
+        }
+        if(empty($return)){
+            echo "Tidak Ada";
+            return;
+        }
+        if($return) {
+            echo "Ada";
+            return;
+        }
+        ?>
+    </body>
+</html>
