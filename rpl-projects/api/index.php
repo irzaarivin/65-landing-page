d04bee1f2886a38152029be48026af68a2b3775e
ItsRealNise <90197402+ItsRealNise@users.noreply.github.com>
2022-08-04 12:53:58 +0700

    Add files via upload

--- rpl-projects/api/index.php
+++ rpl-projects/api/index.php
@@ -1,41 +1,28 @@
 <?php
-
 header('Content-type: application/json');
 
-$data = file_get_contents("http://0.0.0.0:8080/dataSiswaRpl.json");
-
+$data = file_get_contents("http://localhost/dataSiswaRpl.json");
 $data = json_decode($data, true);
-
 $cek = $_GET["keyword"];
 
 if(strtolower($cek) == "all"){
-
-	echo json_encode($data);	return;
-
+	echo json_encode($data);
+	return;
 }
 
 $return = [];
 
 $i = 0;
-
 foreach($data as $index => $siswa) {
-
     if(strpos(strtolower($siswa["field3"]), strtolower($cek)) !== false){
-
     	$return[$i] = $data[$index];
-
     	$i++;
-
     }
-
 }
 
 if(empty($return)){
-
 	echo json_encode(["status" => "failed", "msg" => "Siswa yang dicari tidak ditemukan!"]);
-
 	return;
-
 }
 
 echo json_encode($return);
